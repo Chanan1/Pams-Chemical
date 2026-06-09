942ecf63cf6acd212237cb480741c16a72a5d981
Faklh3 <faklhe1persen@gmail.com>
2026-06-09 14:13:22 +0700

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 import { products } from "@/data/products";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://pamschemical.com";
+  const baseUrl = "https://pimakimagro.com";
 
   // Static pages
   const staticPages = [
